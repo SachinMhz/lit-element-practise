7319952b31a9a6841b91268de024c28046671d98
SachinMhz <sachin.mhz.sm@gmail.com>
2021-02-08 18:28:02 +0545

    Use async-await for firebase operations

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,15 @@
 import firebase from "firebase";
 
-const _get = (path) => {
-  return new Promise(function (resolve, reject) {
-    console.log("retriving from db...");
+const _get = async (path) => {
+  console.log("retriving from db...");
+  try {
     let DataRef = firebase.database().ref(path);
-    DataRef.on("value", (snapshot) => {
-      let data_object = snapshot.val(); //object of objects
-      let data_array = Object.values(data_object); //array of objects
-      if (!data_array) {
-        reject("Something wrong with database");
-      }
-      resolve(data_array);
-    });
-  });
+    let data = await (await DataRef.once("value")).val();
+
+    return Object.values(data);
+  } catch (error) {
+    return error.errorMessage;
+  }
 };
 
 const _post = async (path, data) => {
@@ -39,52 +36,38 @@ const _delete = async (path, data) => {
   await firebase.database().ref(dataPath).remove();
 };
 
-const _signin = (email, password) => {
-  return new Promise(function (resolve, reject) {
-    firebase
+const _signin = async (email, password) => {
+  try {
+    const data = await firebase
       .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        var user = userCredential.user;
-        resolve(user);
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        reject(errorMessage);
-      });
-  });
+      .createUserWithEmailAndPassword(email, password);
+
+    return data.user;
+  } catch (error) {
+    return error.errorMessage;
+  }
 };
 
-const _login = (email, password) => {
-  return new Promise(function (resolve, reject) {
-    firebase
+const _login = async (email, password) => {
+  try {
+    const data = await firebase
       .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        var user = userCredential.user;
-        resolve(user);
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        reject(errorMessage);
-      });
-  });
+      .signInWithEmailAndPassword(email, password);
+
+    return data.user;
+  } catch (error) {
+    return error.errorMessage;
+  }
 };
 
-const _signout = () => {
-  return new Promise(function (resolve, reject) {
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        resolve(true);
-      })
-      .catch((error) => {
-        reject("Failed to log out");
-      });
-  });
+const _signout = async () => {
+  try {
+    await firebase.auth().signOut();
+
+    return true;
+  } catch (error) {
+    return error.errorMessage;
+  }
 };
 
 const Fire = {
